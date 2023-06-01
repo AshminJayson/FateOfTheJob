84abb837029d1c60537e70a91df6b23a7f086940
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-06-01 21:45:55 +0530

    started file upload

--- src/Components/Fileupload.tsx
+++ src/Components/Fileupload.tsx
@@ -1,11 +1,27 @@
 'use client';
 
+import { Container, Input,Flex } from "@chakra-ui/react";
 import styles from "./page.module.css";
 
 export default function App(){
     return (
         <>
-        <div>hey</div>
+        <Flex justify='centre' rowGap={3} flexDir={'column'} margin={3}>
+            
+                <h1>File Upload</h1>
+                <p>Upload your files here</p>
+                 <form action="">
+                    <Input
+                     type="file" 
+                     name="file" 
+                     id="file" 
+                     variant={'outline'}
+                    />
+                 </form>
+            
+            
+        </Flex>
+        
         </>
     )
 }
