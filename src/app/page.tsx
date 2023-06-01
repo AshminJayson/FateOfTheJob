bf2e83d2885b9b8a58967e3c5a32e1611f48032d
Ashmin Jayson <ashminjayson10@gmail.com>
2023-06-01 17:58:18 +0530

    chore: added chakra ui provider and base navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import styles from "./page.module.css";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Home() {
     return (
         <main className={styles.main}>
-            <p>Base Scaffold.</p>
+            <Flex justify="center">
+                <Text fontSize="2xl" fontStyle="italic" fontWeight="bold">
+                    FATE OF THE JOB
+                </Text>
+            </Flex>
         </main>
     );
 }
