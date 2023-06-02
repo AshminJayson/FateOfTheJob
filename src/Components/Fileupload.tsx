f737cf9a3f24cf2e16b39cbf87316ff8540049d3
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-06-02 06:19:37 +0530

    file invisble input

--- src/Components/Fileupload.tsx
+++ src/Components/Fileupload.tsx
@@ -1,23 +1,31 @@
 'use client';
 
-import { Container, Input,Flex } from "@chakra-ui/react";
+import { Container, Input,Flex ,Text, FormControl} from "@chakra-ui/react";
 import styles from "./page.module.css";
+import {useDropzone} from 'react-dropzone'
 
 export default function App(){
     return (
         <>
         <Flex justify='centre' rowGap={3} flexDir={'column'} margin={3}>
             
-                <h1>File Upload</h1>
+                <Text textAlign={'center'}>File Upload </Text>
                 <p>Upload your files here</p>
-                 <form action="">
-                    <Input
-                     type="file" 
-                     name="file" 
-                     id="file" 
-                     variant={'outline'}
-                    />
-                 </form>
+                 <Container w={300} h={300} bg={"blackAlpha.300"} justifyItems={'center'} display={'flex'} alignItems={'center'}>
+                     <FormControl >
+                        <Input
+                         type="file"
+                         name="file"
+                         id="file"
+                         variant={'outline'}
+                         aspectRatio={1/1}
+                         outline={'none'}
+                         w='100%'
+                         h="100%"
+                         opacity={0}
+                        />
+                     </FormControl>
+                 </Container>
             
             
         </Flex>
