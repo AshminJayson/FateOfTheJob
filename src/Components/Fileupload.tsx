294ccf1f7e171f3281810904b30c763b9017477b
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-06-03 06:26:45 +0530

    barebones pdf file uploader

--- src/Components/Fileupload.tsx
+++ src/Components/Fileupload.tsx
@@ -1,32 +1,66 @@
 'use client';
-
-import { Container } from '@chakra-ui/react';
-import React, { useCallback } from 'react';
+import { Box, Container, Flex, Text } from '@chakra-ui/react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-
+import { FaFile } from 'react-icons/fa';
 
 function MyDropzone() {
-  const [files, setFiles] = React.useState<File[]>([]);
-  const [fileNames, setFileNames] = React.useState<string[]>([]);
-  // [
+  const [file, setFile] = useState<File | null>(null);
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    console.log(acceptedFiles);
-    setFiles(acceptedFiles);
-    setFileNames(acceptedFiles.map((file) => file.name));
-    
+    if (acceptedFiles.length > 0) {
+      const selectedFile = acceptedFiles[0];
+      if (selectedFile.type === 'application/pdf') {
+        setFile(selectedFile);
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+        console.log(formData);
+
+        //Add the api call here
+        
+      }
+    }
   }, []);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: false,
+    accept: '.pdf',
+  });
 
   return (
-    <Container {...getRootProps()} bg={'blackAlpha.300'}>
-      <input {...getInputProps()}  />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag n drop some files here, or click to select files</p>
-      )}
-    </Container>
+    <Box
+      {...getRootProps()}
+      bg="blackAlpha.300"
+      m={10}
+      w="30rem"
+      h="30rem"
+      rounded="md"
+      border="2px dashed"
+      borderColor={isDragActive ? 'blue.500' : 'gray.300'}
+      cursor="pointer"
+    >
+      <Flex
+        direction="column"
+        align="center"
+        justify="center"
+        h="100%"
+      >
+        <Box mb={4}>
+          <FaFile size={48} />
+        </Box>
+        {file ? (
+          <Text>{file.name}</Text>
+        ) : (
+          <Text>
+            {isDragActive
+              ? 'Drop the PDF file here...'
+              : 'Drag and drop a PDF file here, or click to select'}
+          </Text>
+        )}
+      </Flex>
+      <input {...getInputProps()} />
+    </Box>
   );
 }
 
