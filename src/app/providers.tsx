0c9c7fefb121abd9f88b19aa866e0ac1d711443a
Ashmin Jayson <ashminjayson10@gmail.com>
2023-06-04 21:13:49 +0530

    chore: switched chakra theme to dark mode

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
+
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <CacheProvider>
-            <ChakraProvider>{children}</ChakraProvider>
+            <ChakraProvider theme={theme}>{children}</ChakraProvider>
         </CacheProvider>
     );
 }
