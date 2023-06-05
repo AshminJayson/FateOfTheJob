758f9dd6841ae85048642330b2920b58dd987377
Ashmin Jayson <ashminjayson10@gmail.com>
2023-06-05 17:45:52 +0530

    feat: added loading animation to button and job suggestion display

--- src/Components/Fileupload.tsx
+++ src/Components/Fileupload.tsx
@@ -2,12 +2,16 @@
 
 import { useCallback, useState } from "react";
 import axios from "axios";
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, useToast } from "@chakra-ui/react";
 import { useDropzone, DropzoneOptions, Accept } from "react-dropzone";
 import { FaFile } from "react-icons/fa";
 
 function MyDropzone() {
     const [file, setFile] = useState<File | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [jobRole, setJobRole] = useState<string | null>(null);
+
+    const toast = useToast();
 
     const onDrop: DropzoneOptions["onDrop"] = useCallback(
         (acceptedFiles: File[]) => {
@@ -15,25 +19,40 @@ function MyDropzone() {
                 const selectedFile = acceptedFiles[0];
                 if (selectedFile.type === "application/pdf") {
                     setFile(selectedFile);
-                    
                 }
             }
         },
         []
     );
 
-    const uploadFile =  () => {
-        if (file){
-                    const formData = new FormData();
-                    formData.append("file", file);
+    const uploadFile = () => {
+        if (file) {
+            const formData = new FormData();
+            formData.append("file", file);
+            setLoading(true);
+            setJobRole(null);
 
-                    axios({
-                        method: "post",
-                        url: "http://localhost:8000/api/files",
-                        data: formData,
-                        headers: { "Content-Type": "multipart/form-data" },
-                    }).then((response) => {console.log(response.data);});
-                }
+            axios({
+                method: "post",
+                url: "http://localhost:8000/api/files",
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data" },
+            })
+                .then((response) => {
+                    setLoading(false);
+                    console.log(response.data.message);
+                    setJobRole(response.data.message);
+                })
+                .catch((err) => {
+                    setLoading(false);
+                    toast({
+                        title: "Error",
+                        description: err.message,
+                        status: "error",
+                        duration: 3000,
+                    });
+                });
+        }
     };
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -44,34 +63,50 @@ function MyDropzone() {
 
     return (
         <>
-        <Box
-            {...getRootProps()}
-            bg="blackAlpha.300"
-            m={10}
-            w="28rem"
-            h="28rem"
-            rounded="md"
-            border="2px dashed"
-            borderColor={isDragActive ? "blue.500" : "gray.300"}
-            cursor="pointer"
-        >
-            <Flex direction="column" align="center" justify="center" h="100%">
-                <Box mb={4}>
-                    <FaFile size={48} />
-                </Box>
-                {file ? (
-                    <Text>{file.name}</Text>
-                ) : (
-                    <Text>
-                        {isDragActive
-                            ? "Drop the PDF file here..."
-                            : "Drag and drop a PDF file here, or click to select"}
-                    </Text>
-                )}
-            </Flex>
-            <input {...getInputProps()} />
-        </Box>
-        <Button onClick={uploadFile}>Submit</Button>
+            <Box
+                {...getRootProps()}
+                bg="blackAlpha.300"
+                m={8}
+                w="28rem"
+                h="28rem"
+                rounded="md"
+                border="2px dashed"
+                borderColor={isDragActive ? "blue.500" : "gray.300"}
+                cursor="pointer"
+            >
+                <Flex
+                    direction="column"
+                    align="center"
+                    justify="center"
+                    h="100%"
+                >
+                    <Box mb={4}>
+                        <FaFile size={48} />
+                    </Box>
+                    {file ? (
+                        <Text>{file.name}</Text>
+                    ) : (
+                        <Text>
+                            {isDragActive
+                                ? "Drop the PDF file here..."
+                                : "Drag and drop a PDF file here, or click to select"}
+                        </Text>
+                    )}
+                </Flex>
+                <input {...getInputProps()} />
+            </Box>
+            {jobRole && (
+                <Text fontWeight="bold" fontSize="1xl" padding="1rem">
+                    Suggested Job Role : {jobRole}
+                </Text>
+            )}
+            <Button
+                isLoading={loading}
+                isDisabled={file == null}
+                onClick={uploadFile}
+            >
+                Submit
+            </Button>
         </>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ import Chatdrawer from "../Components/Chatdrawer";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Flex justify="center">
-        <Text fontSize="2xl" fontStyle="italic" fontWeight="bold">
-          FATE OF THE JOB
-        </Text>
-      </Flex>
-      <Fileupload />
-      <Chatdrawer />
-    </main>
-  );
+    return (
+        <main className={styles.main}>
+            <Flex justify="center">
+                <Text fontSize="2xl" fontStyle="italic" fontWeight="bold">
+                    FATE OF THE JOB
+                </Text>
+            </Flex>
+            <Fileupload />
+            <Chatdrawer />
+        </main>
+    );
 }
